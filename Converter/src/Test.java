56fec7ac3ab5a6fa4602f52c6dd66afc3629b61f
bechir sokni <soknibechir@gmail.com>
2019-03-19 22:48:46 +0100

    update

--- Converter/src/Test.java
+++ Converter/src/Test.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 //import java.io.FileOutputStream;
 import java.io.IOException;
-//import java.io.InputStream;
+import java.io.InputStream;
 ///import java.io.OutputStream;
 
 public class Test {
